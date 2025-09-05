0e859667633c2ddc3c5b279b3ac9a954e411cf5e
Matīss <matiss_rikters@hotmail.com>
2025-09-05 22:51:35 +0900

    History test script

---
+++ run_history_tests.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+pyenv activate phi4
+
+models=("google/gemma-3-4b-it" "Qwen/Qwen2.5-VL-3B-Instruct" "microsoft/Phi-4-multimodal-instruct")
+
+for model in "${models[@]}"; do
+    echo "python test_history_local.py --model"$model" >> output.tsv"
+    # python test_history_local.py --model $model --history 5 >> history.tsv
+    python test_history_local.py --model $model --history 1 >> history.tsv
+    python test_history_local.py --model $model --history 0 >> history.tsv
+done
+
+pyenv activate phi3
+
+models=("microsoft/Phi-3.5-vision-instruct")
+
+for model in "${models[@]}"; do
+    echo "python test_history_local.py --model"$model" >> output.tsv"
+    # python test_history_local.py --model $model --history 5 >> history.tsv
+    python test_history_local.py --model $model --history 1 >> history.tsv
+    python test_history_local.py --model $model --history 0 >> history.tsv
+done
+
+pyenv activate appl
+
+models=("apple/FastVLM-0.5B" "apple/FastVLM-1.5B" "apple/FastVLM-7B")
+
+for model in "${models[@]}"; do
+    echo "python test_history_local.py --model"$model" >> output.tsv"
+    # python test_history_local.py --model $model --history 5 >> history.tsv
+    python test_history_local.py --model $model --history 1 >> history.tsv
+    python test_history_local.py --model $model --history 0 >> history.tsv
+done
